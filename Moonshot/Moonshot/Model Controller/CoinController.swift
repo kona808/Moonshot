a0fdaec07a35e2a9126465969a5f12d6ddb0a548
Jacob I  Perez <jacobperez91@gmail.com>
2022-10-03 10:50:09 -1000

    d, t, c

--- Moonshot/Moonshot/Model Controller/CoinController.swift
+++ Moonshot/Moonshot/Model Controller/CoinController.swift
@@ -20,6 +20,30 @@ class CoinController: Codable {
         let coinsURL = baseURL.appendingPathComponent(keyCoinsComponents)
         let finalURL = coinsURL.appendingPathComponent(keyListComponents)
         print(finalURL)
+        
+        URLSession.shared.dataTask(with: finalURL) { coinData, _, error in
+            if let error = error {
+                print("There was an error: \(error.localizedDescription)")
+                completion(false)
+            }
+            guard let data = coinData else { completion(false)
+                return }
+            do {
+                if let topLevelArrayOfCoinDictionaries = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String: String]] {
+                    for coinDictionary in topLevelArrayOfCoinDictionaries {
+                        if let id = coinDictionary["id"],
+                           let symbol = coinDictionary["symbol"],
+                           let name = coinDictionary["name"] {
+                            let parcedCoin = Coin(id: id, symbol: symbol, name: name)
+                            coins.append(parcedCoin)
+                        }
+                    }
+                    completion(true)
+                }
+            } catch {
+                print("Error in Do/Try/Catch: \(error.localizedDescription)")
+                    completion(false)
+            }
+        }.resume()
     }
-    
 } // End of class
